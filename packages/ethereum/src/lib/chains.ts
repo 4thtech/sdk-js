b5e1057ddf535929d53c910672f2e78e98fda188
Denis Jazbec <denis.jazbec@smartee.io>
2024-05-27 17:56:02 +0200

    Update hardhat contract addresses

--- packages/ethereum/src/lib/chains.ts
+++ packages/ethereum/src/lib/chains.ts
@@ -19,13 +19,13 @@ export const hardhat: Chain = {
       address: '0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512',
     },
     mail: {
-      address: '0xa513E6E4b8f2a923D98304ec87F64353C4D5C853',
+      address: '0x5FC8d32690cc91D4c39d9d3abcBD16989F875707',
     },
     chat: {
-      address: '0x610178dA211FEF7D417bC0e6FeD39F05609AD788',
+      address: '0xa513E6E4b8f2a923D98304ec87F64353C4D5C853',
     },
     user: {
-      address: '0xDc64a140Aa3E981100a9becA4E685f962f0cF6C9',
+      address: '0xCf7Ed3AccA5a467e9e704C703E8D87F634fB0Fc9',
     },
   },
 };
