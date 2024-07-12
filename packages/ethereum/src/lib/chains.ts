b3822365be94f03e2ddb4d4f1bab952a874ae2aa
Denis Jazbec <denis.jazbec@smartee.io>
2024-07-12 21:58:28 +0200

    Add Fantom chain config

--- packages/ethereum/src/lib/chains.ts
+++ packages/ethereum/src/lib/chains.ts
@@ -1,3 +1,4 @@
+export { fantom } from './chains/fantom';
 export { fantomTestnet } from './chains/fantomTestnet';
 export { hardhat } from './chains/hardhat';
 export { immu3Testnet } from './chains/immu3Testnet';
---
+++ packages/ethereum/src/lib/chains/fantom.ts
@@ -0,0 +1,24 @@
+import { Chain } from '@4thtech-sdk/types';
+import { fantom as viemFantom } from 'viem/chains';
+
+export const fantom: Chain = {
+  ...viemFantom,
+  rpcUrls: {
+    default: { http: ['https://rpc.ftm.tools'] },
+    public: { http: ['https://rpc.ftm.tools'] },
+  },
+  contracts: {
+    appFeeManager: {
+      address: '0x54FF47513810F1beE9FE3D1d55d3EAfdb741F54d',
+    },
+    mail: {
+      address: '0x39Dc65b90e9C96828213E1c10073a3Ac9ee00777',
+    },
+    chat: {
+      address: '0xe8150FcEeF266CF81f4dD936c786E4a041be2d6b',
+    },
+    user: {
+      address: '0x6BC7B159a8d97B907B2F1912769DE72FE8b773A0',
+    },
+  },
+};
