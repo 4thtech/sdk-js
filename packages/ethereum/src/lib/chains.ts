cf29002218af24015126d922f77704c20cfb7a9c
Denis Jazbec <denis.jazbec@smartee.io>
2024-09-03 10:44:10 +0200

    Add Minato testnet chain config

--- packages/ethereum/src/lib/chains.ts
+++ packages/ethereum/src/lib/chains.ts
@@ -2,4 +2,5 @@ export { fantom } from './chains/fantom';
 export { fantomTestnet } from './chains/fantomTestnet';
 export { hardhat } from './chains/hardhat';
 export { immu3Testnet } from './chains/immu3Testnet';
+export { minato } from './chains/minato';
 export { oasisSaphire } from './chains/oasisSaphire';
