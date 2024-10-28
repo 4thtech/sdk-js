c6d116f3f5e92b0d83033a544f825d3b7c7f67bb
Denis Jazbec <denis.jazbec@smartee.io>
2024-10-28 14:12:25 +0100

    Add gnosisChiado and polygonAmoy chain export

--- packages/ethereum/src/lib/chains.ts
+++ packages/ethereum/src/lib/chains.ts
@@ -1,6 +1,8 @@
 export { fantom } from './chains/fantom';
 export { fantomTestnet } from './chains/fantomTestnet';
+export { gnosisChiado } from './chains/gnosisChiado';
 export { hardhat } from './chains/hardhat';
 export { immu3Testnet } from './chains/immu3Testnet';
 export { minato } from './chains/minato';
 export { oasisSaphire } from './chains/oasisSaphire';
+export { polygonAmoy } from './chains/polygonAmoy';
