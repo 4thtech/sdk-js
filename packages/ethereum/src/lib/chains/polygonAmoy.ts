d541235454af0b42219711fcf6d12b57f371c862
Denis Jazbec <denis.jazbec@smartee.io>
2024-10-28 13:26:55 +0100

    Add Polygon Amoy testnet chain config

---
+++ packages/ethereum/src/lib/chains/polygonAmoy.ts
@@ -0,0 +1,38 @@
+import { Chain } from '@4thtech-sdk/types';
+import { defineChain } from 'viem';
+
+export const polygonAmoy: Chain = defineChain({
+  id: 80_002,
+  name: 'Polygon Amoy',
+  network: 'polygon-amoy',
+  nativeCurrency: { name: 'MATIC', symbol: 'MATIC', decimals: 18 },
+  rpcUrls: {
+    default: {
+      http: ['https://rpc-amoy.polygon.technology'],
+    },
+    public: {
+      http: ['https://rpc-amoy.polygon.technology'],
+    },
+  },
+  blockExplorers: {
+    default: {
+      name: 'PolygonScan',
+      url: 'https://amoy.polygonscan.com',
+    },
+  },
+  contracts: {
+    appFeeManager: {
+      address: '0x54FF47513810F1beE9FE3D1d55d3EAfdb741F54d',
+    },
+    mail: {
+      address: '0x39Dc65b90e9C96828213E1c10073a3Ac9ee00777',
+    },
+    chat: {
+      address: '0xe8150FcEeF266CF81f4dD936c786E4a041be2d6b',
+    },
+    user: {
+      address: '0x6BC7B159a8d97B907B2F1912769DE72FE8b773A0',
+    },
+  },
+  testnet: true,
+});
